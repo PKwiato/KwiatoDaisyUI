300a3e4593524b4c87f5d62fbd4f60f018630a4f
Piotr Kwiatkowski <piotr.kwiatkowski@pb.com>
2025-06-19 11:06:05 +0200

    adding navi bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./core/layout/footer/footer.component";
 import { HeaderComponent } from './core/layout/header/header.component';
+import { NavibarComponent } from './core/layout/navibar/navibar/navibar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, HeaderComponent, NavibarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'KwiatoDaisyUI';
+  title = 'KwiatoWebPage';
 }
