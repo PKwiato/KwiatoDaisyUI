2425c5fdd7dca6ffc10c761059d0b84804d77b43
Piotr Kwiatkowski <piotr.kwiatkowski@pb.com>
2025-06-22 11:04:31 +0200

    refactoring component name so it reflect proper use

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from "./core/layout/footer/footer.component";
-import { HeaderComponent } from './core/layout/header/header.component';
+import { HomeComponent } from './core/layout/home/home.component';
 import { NavibarComponent } from './core/layout/navibar/navibar/navibar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, HeaderComponent, NavibarComponent],
+  imports: [RouterOutlet, FooterComponent, HomeComponent, NavibarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
